71e5e544aa1d756651c3a4bf650f7557752da373
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2022-10-25 12:00:42 +0900

    commit

---
+++ delivery/src/main/java/untitled/domain/ShippingRepository.java
@@ -0,0 +1,9 @@
+package untitled.domain;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import untitled.domain.*;
+
+@RepositoryRestResource(collectionResourceRel = "shippings", path = "shippings")
+public interface ShippingRepository
+    extends PagingAndSortingRepository<Shipping, Long> {}
---
+++ order/src/main/java/untitled/domain/Order.java
@@ -0,0 +1,63 @@
+package untitled.domain;
+
+import untitled.domain.OrderPlaced;
+import untitled.OrderApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Order_table")
+@Data
+
+public class Order  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private Long productid;
+    
+    
+    
+    
+    
+    private Long qty;
+    
+    
+    
+    
+    
+    private Long customerid;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+    }
+
+    public static OrderRepository repository(){
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+
+
+
+
+
+}
